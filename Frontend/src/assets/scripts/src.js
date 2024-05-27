3ec3c34bd0cc48b59733377d72a34329e523e8f0
Armin Đidelija <didelija.armin@gmail.com>
2024-05-27 19:46:23 +0200

    update mobile on user panel

--- Frontend/src/assets/scripts/src.js
+++ Frontend/src/assets/scripts/src.js
@@ -2,6 +2,24 @@ function init_plugin(){
   $(document).ready(function () {
     $('#sidebarCollapse').on('click', function () {
       $('#sidebar').toggleClass('active');
+      if(!upaljen && window.innerWidth<=768)
+        upaljen=!upaljen;
+    });
+  });
+
+  var upaljen=false;
+  $(document).ready(function () {
+    $('#sidebarCollapse1').on('click', function () {
+      upaljen=!upaljen;
+      $('#sidebar').toggleClass('active');
+    });
+  });
+  $(document).ready(function () {
+    $('.liClick').on('click', function () {
+      if(upaljen && window.innerWidth<=768){
+        $('#sidebar').toggleClass('active');
+        upaljen=!upaljen;
+      }
     });
   });
 }
